862c27dd9a6c078b75c2653dcbc4884d1bc092b0
alonsoazz04 <alonsoantonio.zamora@alu.uclm.es>
2026-05-11 13:37:59 +0200

    Modificación de worker_buscador para la Práctica 3

--- src/worker_buscador.cpp
+++ src/worker_buscador.cpp
@@ -1,12 +1,16 @@
 #include "worker_buscador.hpp"
 #include "procesador_linea.hpp"
 
-
 void WorkerBuscador::ejecutar(Buscador local, const std::vector<std::string>& lineas, ResultadosGlobales& globales) {
-    for (int i = local.inicio; i <= local.fin; ++i) {
-        std::vector<ResultadoBusqueda> encontrados = ProcesadorLinea::buscarEnLinea(lineas[i], local.palabra, i+1);
-        local.resultados.insert(local.resultados.end(), encontrados.begin(), encontrados.end());
+    for (std::size_t i = 0; i < lineas.size(); ++i) {
+        std::vector<ResultadoBusqueda> encontrados =
+            ProcesadorLinea::buscarEnLinea(lineas[i], local.palabra, static_cast<int>(i + 1));
+
+        for (auto& resultado : encontrados) {
+            resultado.libro = local.libro;
+            local.resultados.push_back(resultado);
+        }
     }
 
     publish(globales, std::move(local));
-}  
+}
